e2afe7ffb836d3d6c363247ae0a9c545ee2c4c82
amgfernandes <amgfernandes@gmail.com>
2021-11-04 11:47:23 +0100

    testing pygad

---
+++ Reproducing Images.py
@@ -0,0 +1,55 @@
+# Reproducing Images
+''' see https://pygad.readthedocs.io/en/latest/README_pygad_ReadTheDocs.html#examples'''
+# %%
+
+import imageio
+import numpy
+import gari
+import pygad
+# %%
+
+target_im = imageio.imread('fruit.jpg')
+target_im = numpy.asarray(target_im/255, dtype=numpy.float64)
+
+# %%
+
+target_chromosome = gari.img2chromosome(target_im)
+
+def fitness_fun(solution, solution_idx):
+    fitness = numpy.sum(numpy.abs(target_chromosome-solution))
+
+    # Negating the fitness value to make it increasing rather than decreasing.
+    fitness = numpy.sum(target_chromosome) - fitness
+    return fitness
+# %%
+
+ga_instance = pygad.GA(num_generations=20000,
+                       num_parents_mating=10,
+                       fitness_func=fitness_fun,
+                       sol_per_pop=20,
+                       num_genes=target_im.size,
+                       init_range_low=0.0,
+                       init_range_high=1.0,
+                       mutation_percent_genes=0.01,
+                       mutation_type="random",
+                       mutation_by_replacement=True,
+                       random_mutation_min_val=0.0,
+                       random_mutation_max_val=1.0)
+# %%
+ga_instance.run()
+# %%
+ga_instance.plot_fitness()
+# %%
+# Returning the details of the best solution.
+solution, solution_fitness, solution_idx = ga_instance.best_solution()
+print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+print("Index of the best solution : {solution_idx}".format(solution_idx=solution_idx))
+
+if ga_instance.best_solution_generation != -1:
+    print("Best fitness value reached after {best_solution_generation} generations.".format(best_solution_generation=ga_instance.best_solution_generation))
+
+result = gari.chromosome2img(solution, target_im.shape)
+matplotlib.pyplot.imshow(result)
+matplotlib.pyplot.title("PyGAD & GARI for Reproducing Images")
+matplotlib.pyplot.show()
+# %%
